0895ebec65ab73fc14ccfdd8c8784275106ad770
Dmytro Baziaka <dmytro_baziaka@epam.com>
2019-05-21 13:26:52 +0300

    Returned test and exception added.

--- src/main/java/ua/baziaka/problems/TriangleMath.java
+++ src/main/java/ua/baziaka/problems/TriangleMath.java
@@ -3,7 +3,7 @@ package ua.baziaka.problems;
 public class TriangleMath {
     public boolean isEquilateral(int x, int y, int z) {
         if (true){
-            throw new RuntimeExeption("exception caused :*(");
+            throw new RuntimeException("Not implemeted.");
         }
         return x > 0 && x == y && y == z;
     }
